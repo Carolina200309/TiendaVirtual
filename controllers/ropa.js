f86e671fc5efe751d3e7b3196ceed9ae1b020463
Carolina200309 <100418076+Carolina200309@users.noreply.github.com>
2024-09-29 11:54:05 -0500

    Creacion de autenticación de usuario y asignacion de roles

--- controllers/ropa.js
+++ controllers/ropa.js
@@ -4,25 +4,55 @@
 const Ropa = require('../models/ropa'); 
 
 function crearRopa(req, res) {
-    var ropaRecibida = req.body;
 
-    var nuevaRopa = new Ropa({
-        tipoRopa: ropaRecibida.tipoRopa,
-        nombre: ropaRecibida.nombre,
-        color: ropaRecibida.color,
-        precio: ropaRecibida.precio,
-        talla: ropaRecibida.talla,
-        material: ropaRecibida.material
+    var usuarioId = req.userId;  
+    var usuarioRol = req.userRol;
+
+    console.log("Rol del usuario: ", usuarioRol);
+
+    // Verificar si el usuario tiene rol de administrador
+    if (usuarioRol === 'administrador') 
+        {
+
+        var ropaRecibida = req.body;
+
+        var nuevaRopa = new Ropa({
+            tipoRopa: ropaRecibida.tipoRopa,
+            nombre: ropaRecibida.nombre,
+            color: ropaRecibida.color,
+            precio: ropaRecibida.precio,
+            talla: ropaRecibida.talla,
+            material: ropaRecibida.material
+        });
+
+        nuevaRopa.save().then(
+            (ropaGuardada) => {
+                res.status(200).send({ ropaCreada: ropaGuardada });
+            },
+            (err) => {
+                res.status(500).send({
+                    message: 'No se pudo crear, intente nuevamente',
+                    error: err
+                });
+            }
+        );
+    }
+    else {
+    // Si el usuario no es administrador, devolver un error
+    res.status(403).send({
+        message: 'No está autorizado para crear ropa nueva.'
     });
+}
+}
 
-    nuevaRopa.save().then(
-        (ropaGuardada) => {
-            res.status(200).send({ ropaCreada: ropaGuardada });
+function consultarRopa(req,resp){
+    Ropa.find({}).then(
+        (ropa)=>{
+            resp.status(200).send({ropa: ropa });
         },
         (err) => {
-            res.status(500).send({
-                message: 'No se pudo crear, intente nuevamente',
-                error: err
+            resp.status(500).send({
+                message: 'No se pudo obtener la ropa. Intente nuevamente.'
             });
         }
     );
@@ -30,4 +60,5 @@ function crearRopa(req, res) {
 
 module.exports = {
     crearRopa,
+    consultarRopa
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,19 @@
 
 var express = require('express');
 var ropaController = require('../controllers/ropa');
+var token = require('../helpers/autenticacion');
+const usuarios = require('../models/usuarios');
+var authenticationController = require("../controllers/autenticacion");
+
 var routes = express.Router();
 
 
-routes.post('/api/ropa', ropaController.crearRopa);
+routes.post('/api/ropa', token.validarToken, ropaController.crearRopa);
+
+routes.post('/api/usuario', authenticationController.registrarUsuario);
+
+routes.post('/api/login', authenticationController.iniciarSesion);
+
+routes.get('/api/ropa',token.validarToken, ropaController.consultarRopa);
 
 module.exports = routes;
