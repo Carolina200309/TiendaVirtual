8cae8ae7061349649aaa9d1ac6bd9eca126d31e4
Juan <jm.arango03@gmail.com>
2024-10-11 22:27:59 -0500

    Create default users

--- index.js
+++ index.js
@@ -2,19 +2,38 @@
 
 var application = require('./application');
 var mongoose = require('mongoose');
+
+const Usuario = require('./models/usuarios');
+const bcrypt = require("bcryptjs");
+
 mongoose.connect(
     "mongodb://127.0.0.1:27017/tiendavirtual")
     .then(
         () => {
             console.log("Conexion exitosa");
             application.listen(9898, function(){
-                console.log("Aplicacion inciada");
-            })
 
+                checkUserRoles("administrador", "distribuidor")
+
+                console.log("Aplicacion inciada");
+            });
         },
         err => {
             console.log("Conexion con BBDD fallida.");
         }
     );
 
-    
+function checkUserRoles(...roles) {
+    const salt = bcrypt.genSaltSync(10);
+    roles.forEach(role => {
+        const query = Usuario.findOne({rol: role})
+
+        if (!query) {
+            const newUser = new Usuario({
+                username: role,
+                password: bcrypt.hashSync("123", salt),
+                rol: role,
+            }).save();
+        }
+    });
+}
